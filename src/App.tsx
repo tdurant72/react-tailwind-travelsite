ffe9daf53c3d6155af6ef0b7f626562a605da34d
Tyus Durant <contact@tyusdurant.com>
2021-10-19 16:53:27 -0400

    init 101921

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import tw from "twin.macro"
+import { TopSection } from './containers/TopSection';
+import { ExploreSection } from './containers/ExploreSection';
+import { Footer } from './components/Footer';
+const AppContainer = tw.div`
+  flex
+  flex-col
+  w-full
+  h-full
+  overflow-hidden
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <AppContainer><TopSection /><ExploreSection /><Footer /></AppContainer>
+
 }
 
 export default App;
